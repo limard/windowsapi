00c26a99e2e9eb4048105674b59bac92574765d0
limard <limard@gmail.com>
2018-05-04 11:34:48 +0800

    amd64时，不执行Wow64DisableWow64FsRedirection

--- redirection64.go
+++ redirection64.go
@@ -3,23 +3,35 @@
 package windowsapi
 
 import (
+	"runtime"
 	"unsafe"
 )
 
 func Wow64DisableWow64FsRedirection() (oldvalue uintptr, err error) {
-	if err = pWow64DisableWow64FsRedirection.Find(); err != nil {
+	// This function is useful for 32-bit applications that want to gain access to the native system32 directory.
+	if runtime.GOARCH == "amd64" {
 		return
 	}
 
+	if err = pWow64DisableWow64FsRedirection.Find(); err != nil {
+		return
+	}
+	//log.Printf("%v", unsafe.Sizeof(oldvalue))
 	ret, _, err := pWow64DisableWow64FsRedirection.Call(uintptr(unsafe.Pointer(&oldvalue)))
 	if ret == 0 {
 		return
 	}
 
-	return oldvalue, nil
+	// If the function succeeds, the return value is a nonzero value.
+	err = nil
+	return
 }
 
 func Wow64EnableWow64FsRedirection(enable uint) (err error) {
+	if runtime.GOARCH == "amd64" {
+		return
+	}
+
 	if err = pWow64EnableWow64FsRedirection.Find(); err != nil {
 		return
 	}
@@ -33,6 +45,10 @@ func Wow64EnableWow64FsRedirection(enable uint) (err error) {
 }
 
 func Wow64RevertWow64FsRedirection(oldValue uintptr) (err error) {
+	if runtime.GOARCH == "amd64" {
+		return
+	}
+
 	if err = pWow64RevertWow64FsRedirection.Find(); err != nil {
 		return
 	}
